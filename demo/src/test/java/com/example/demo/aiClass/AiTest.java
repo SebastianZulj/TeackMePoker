1a15b9073476b4978af132037c3d3f50f7b07174
Sebastian Zulj <zulj98.sebastian@gmail.com>
2024-02-23 11:05:35 +0100

    Testfall 9-14

--- demo/src/test/java/com/example/demo/aiClass/AiTest.java
+++ demo/src/test/java/com/example/demo/aiClass/AiTest.java
@@ -77,8 +77,122 @@ class AiTest {
     @Test
     void TF9() {
         Ai sebZul = new Ai(50, "Sebastian Zulj");
-
+        Card[] flop = new Card[3];
+        Card card1 = new Card(Suit.HEARTS, CardValue.TWO, null);
+        Card card2 = new Card(Suit.SPADES, CardValue.TWO, null);
+        flop[0] = new Card(Suit.DIAMONDS, CardValue.TWO, null);
+        flop[1] = new Card(Suit.CLUBS, CardValue.FOUR, null);
+        flop[2] = new Card(Suit.DIAMONDS, CardValue.THREE, null);
+        sebZul.setStartingHand(card1, card2);
+        sebZul.makeDecision(10 ,flop);
+        String decision = sebZul.getDecision();
+        if (decision.contains(",")){
+            String[] temp = new String[4];
+            temp = decision.split(",");
+            decision = temp[0];
+        }
+        assertEquals("raise", decision);
     }
 
+    @Test
+    void TF10() {
+        Ai sebZul = new Ai(50, "Sebastian Zulj");
+        Card[] flop = new Card[3];
+        Card card1 = new Card(Suit.HEARTS, CardValue.TWO, null);
+        Card card2 = new Card(Suit.HEARTS, CardValue.THREE, null);
+        flop[0] = new Card(Suit.HEARTS, CardValue.FOUR, null);
+        flop[1] = new Card(Suit.HEARTS, CardValue.FIVE, null);
+        flop[2] = new Card(Suit.HEARTS, CardValue.SIX, null);
+        sebZul.setStartingHand(card1, card2);
+        sebZul.makeDecision(10 ,flop);
+        String decision = sebZul.getDecision();
+        if (decision.contains(",")){
+            String[] temp = new String[4];
+            temp = decision.split(",");
+            decision = temp[0];
+        }
+        assertEquals("raise", decision);
+    }
+
+    @Test
+    void TF11() {
+        Ai sebZul = new Ai(50, "Sebastian Zulj");
+        Card[] flop = new Card[3];
+        Card card1 = new Card(Suit.HEARTS, CardValue.TWO, null);
+        Card card2 = new Card(Suit.HEARTS, CardValue.FOUR, null);
+        flop[0] = new Card(Suit.HEARTS, CardValue.SIX, null);
+        flop[1] = new Card(Suit.HEARTS, CardValue.EIGHT, null);
+        flop[2] = new Card(Suit.HEARTS, CardValue.TEN, null);
+        sebZul.setStartingHand(card1, card2);
+        sebZul.makeDecision(10 ,flop);
+        String decision = sebZul.getDecision();
+        if (decision.contains(",")){
+            String[] temp = new String[4];
+            temp = decision.split(",");
+            decision = temp[0];
+        }
+        assertEquals("raise", decision); //Borde vara raise men ger call.
+    }
+
+    @Test
+    void TF12() {
+        Ai sebZul = new Ai(50, "Sebastian Zulj");
+        Card[] flop = new Card[3];
+        Card card1 = new Card(Suit.HEARTS, CardValue.TWO, null);
+        Card card2 = new Card(Suit.SPADES, CardValue.TWO, null);
+        flop[0] = new Card(Suit.CLUBS, CardValue.TWO, null);
+        flop[1] = new Card(Suit.HEARTS, CardValue.FOUR, null);
+        flop[2] = new Card(Suit.SPADES, CardValue.FOUR, null);
+        sebZul.setStartingHand(card1, card2);
+        sebZul.makeDecision(10 ,flop);
+        String decision = sebZul.getDecision();
+        if (decision.contains(",")){
+            String[] temp = new String[4];
+            temp = decision.split(",");
+            decision = temp[0];
+        }
+        assertEquals("raise", decision); //Borde vara raise men ger call.
+    }
+
+    @Test
+    void TF13() {
+
+        Ai sebZul = new Ai(50, "Sebastian Zulj");
+        Card[] flop = new Card[3];
+        Card card1 = new Card(Suit.HEARTS, CardValue.TWO, null);
+        Card card2 = new Card(Suit.SPADES, CardValue.TWO, null);
+        flop[0] = new Card(Suit.CLUBS, CardValue.TWO, null);
+        flop[1] = new Card(Suit.DIAMONDS, CardValue.TWO, null);
+        flop[2] = new Card(Suit.SPADES, CardValue.FOUR, null);
+        sebZul.setStartingHand(card1, card2);
+        sebZul.makeDecision(10, flop);
+        String decision = sebZul.getDecision();
+        if (decision.contains(",")) {
+            String[] temp = new String[4];
+            temp = decision.split(",");
+            decision = temp[0];
+        }
+        assertEquals("raise", decision);
+    }
+
+    @Test
+    void TF14() {
+        Ai sebZul = new Ai(50, "Sebastian Zulj");
+        Card[] flop = new Card[3];
+        Card card1 = new Card(Suit.HEARTS, CardValue.TWO, null);
+        Card card2 = new Card(Suit.HEARTS, CardValue.THREE, null);
+        flop[0] = new Card(Suit.HEARTS, CardValue.FOUR, null);
+        flop[1] = new Card(Suit.HEARTS, CardValue.FIVE, null);
+        flop[2] = new Card(Suit.HEARTS, CardValue.SIX, null);
+        sebZul.setStartingHand(card1, card2);
+        sebZul.makeDecision(10 ,flop);
+        String decision = sebZul.getDecision();
+        if (decision.contains(",")){
+            String[] temp = new String[4];
+            temp = decision.split(",");
+            decision = temp[0];
+        }
+        assertEquals("raise", decision); //Borde vara raise men ger call.
+    }
 
 }
--- demo/src/test/java/com/example/demo/hand/HandCalculationTest.java
+++ demo/src/test/java/com/example/demo/hand/HandCalculationTest.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class HandCalculationTest {
+class   HandCalculationTest {
     @Test
     void checkPairAndMore() {
     }
